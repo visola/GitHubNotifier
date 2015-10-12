a7eae9977e984030359104bba8863768e307028c
Vinicius Isola <viniciusisola@gmail.com>
2015-10-12 12:09:39 -0400

    Add remove repo menu

--- src/main/java/com/github/visola/githubnotifier/data/RepositoryRepository.java
+++ src/main/java/com/github/visola/githubnotifier/data/RepositoryRepository.java
@@ -1,9 +1,13 @@
 package com.github.visola.githubnotifier.data;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.github.visola.githubnotifier.model.Repository;
 
 public interface RepositoryRepository extends PagingAndSortingRepository<Repository, String>{
+
+  @Transactional
+  void deleteByName(String name);
 
 }
