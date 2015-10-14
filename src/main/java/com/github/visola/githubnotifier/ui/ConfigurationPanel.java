4deadb9d90be2228d27983a6c9782593937a6c54
Vinicius Isola <viniciusisola@gmail.com>
2015-10-14 15:18:03 -0400

    Improve configuartion panel looks

--- src/main/java/com/github/visola/githubnotifier/ui/ConfigurationPanel.java
+++ src/main/java/com/github/visola/githubnotifier/ui/ConfigurationPanel.java
@@ -1,6 +1,8 @@
 package com.github.visola.githubnotifier.ui;
 
-import java.awt.GridLayout;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
 
 import javax.swing.BorderFactory;
 import javax.swing.JLabel;
@@ -12,6 +14,8 @@ import com.github.visola.githubnotifier.model.Configuration;
 
 public class ConfigurationPanel extends JPanel {
 
+  private static final int COLUMN_COUNT = 50;
+
   private static final long serialVersionUID = 1L;
 
   private JTextField githubUrlTextField = new JTextField();
@@ -19,17 +23,56 @@ public class ConfigurationPanel extends JPanel {
   private JTextField usernameTextField = new JTextField();
 
   public ConfigurationPanel() {
-    setLayout(new GridLayout(3, 2));
     setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
 
-    add(new JLabel("GitHub URL"));
-    add(githubUrlTextField);
+    setLayout(new GridBagLayout());
 
-    add(new JLabel("Username"));
-    add(usernameTextField);
+    GridBagConstraints gbcGithubUrlLabel = new GridBagConstraints();
+    gbcGithubUrlLabel.anchor = GridBagConstraints.EAST;
+    gbcGithubUrlLabel.insets = new Insets(0, 0, 5, 5);
+    gbcGithubUrlLabel.gridx = 0;
+    gbcGithubUrlLabel.gridy = 0;
+    JLabel githubUrlLabel = new JLabel("GitHub URL");
+    add(githubUrlLabel, gbcGithubUrlLabel);
 
-    add(new JLabel("Password"));
-    add(passwordTextField);
+    GridBagConstraints gbcGithubUrlTextField = new GridBagConstraints();
+    gbcGithubUrlTextField.fill = GridBagConstraints.BOTH;
+    gbcGithubUrlTextField.insets = new Insets(0, 0, 5, 0);
+    gbcGithubUrlTextField.gridx = 1;
+    gbcGithubUrlTextField.gridy = 0;
+    githubUrlTextField.setColumns(COLUMN_COUNT);
+    add(githubUrlTextField, gbcGithubUrlTextField);
+
+    GridBagConstraints gbcUsernameLabel = new GridBagConstraints();
+    gbcUsernameLabel.anchor = GridBagConstraints.EAST;
+    gbcUsernameLabel.insets = new Insets(0, 0, 5, 5);
+    gbcUsernameLabel.gridx = 0;
+    gbcUsernameLabel.gridy = 1;
+    JLabel userNameLabel = new JLabel("Username");
+    add(userNameLabel, gbcUsernameLabel);
+
+    GridBagConstraints gbcUsernameTextField = new GridBagConstraints();
+    gbcUsernameTextField.fill = GridBagConstraints.BOTH;
+    gbcUsernameTextField.insets = new Insets(0, 0, 5, 0);
+    gbcUsernameTextField.gridx = 1;
+    gbcUsernameTextField.gridy = 1;
+    usernameTextField.setColumns(COLUMN_COUNT);
+    add(usernameTextField, gbcUsernameTextField);
+
+    GridBagConstraints gbcPasswordLabel = new GridBagConstraints();
+    gbcPasswordLabel.anchor = GridBagConstraints.EAST;
+    gbcPasswordLabel.insets = new Insets(0, 0, 0, 5);
+    gbcPasswordLabel.gridx = 0;
+    gbcPasswordLabel.gridy = 2;
+    JLabel passwordLabel = new JLabel("Password");
+    add(passwordLabel, gbcPasswordLabel);
+
+    GridBagConstraints gbcPasswordTextField = new GridBagConstraints();
+    gbcPasswordTextField.fill = GridBagConstraints.BOTH;
+    gbcPasswordTextField.gridx = 1;
+    gbcPasswordTextField.gridy = 2;
+    passwordTextField.setColumns(COLUMN_COUNT);
+    add(passwordTextField, gbcPasswordTextField);
   }
 
   public Configuration getConfiguration() {
