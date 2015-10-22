c33637766a2896e7fae8ba86cb873e71f52cf960
Vinicius Isola <viniciusisola@gmail.com>
2015-10-22 10:30:53 -0400

    Fix #11 - Sort repositories by name

--- src/main/java/com/github/visola/githubnotifier/data/RepositoryRepository.java
+++ src/main/java/com/github/visola/githubnotifier/data/RepositoryRepository.java
@@ -1,5 +1,8 @@
 package com.github.visola.githubnotifier.data;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -9,5 +12,8 @@ public interface RepositoryRepository extends PagingAndSortingRepository<Reposit
 
   @Transactional
   void deleteByName(String name);
+
+  @Query("SELECT r FROM Repository r ORDER BY r.fullName")
+  List<Repository> findAllOrderByFullName();
 
 }
