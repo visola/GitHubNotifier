28d0cac1f815c9cc0d942d35cf0c719f0cd161f4
Vinicius Isola <viniciusisola@gmail.com>
2016-07-29 21:30:36 -0400

    Initial work on fetching pull request data

--- src/main/java/com/github/visola/githubnotifier/GitHubNotifier.java
+++ src/main/java/com/github/visola/githubnotifier/GitHubNotifier.java
@@ -8,13 +8,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
-import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.TaskScheduler;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+import com.github.visola.githubnotifier.schedule.EventPuller;
 import com.github.visola.githubnotifier.ui.SystemTrayManager;
 
-@EnableScheduling
 @SpringBootApplication
 public class GitHubNotifier extends WebMvcConfigurerAdapter {
 
@@ -25,11 +26,17 @@ public class GitHubNotifier extends WebMvcConfigurerAdapter {
       .run(args);
 
     context.getBean(SystemTrayManager.class);
+    context.getBean(EventPuller.class);
   }
 
   @Bean
   public RestTemplate restTemplate() {
     return new RestTemplate();
+  }
+
+  @Bean
+  public TaskScheduler taskScheduler() {
+    return new ThreadPoolTaskScheduler();
   }
 
 }
