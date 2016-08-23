37c8d91815a1b75d3a98b50bde19bd55c4e6fa48
Vinicius Isola <viniciusisola@gmail.com>
2016-08-22 20:21:53 -0400

    Add query runnuer

--- src/main/java/com/github/visola/githubnotifier/GitHubNotifier.java
+++ src/main/java/com/github/visola/githubnotifier/GitHubNotifier.java
@@ -1,7 +1,9 @@
 package com.github.visola.githubnotifier;
 
 import org.springframework.beans.BeansException;
-import org.springframework.boot.WebApplicationType;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ApplicationContext;
@@ -10,10 +12,11 @@ import org.springframework.scheduling.TaskScheduler;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.web.client.RestTemplate;
 
+import com.github.visola.githubnotifier.data.QueryRunner;
 import com.github.visola.githubnotifier.service.ConfigurationService;
 
 @SpringBootApplication
-public class GitHubNotifier {
+public class GitHubNotifier extends WebMvcConfigurerAdapter implements ApplicationRunner {
 
   public static void main(String[] args) throws BeansException {
     new SpringApplicationBuilder(GitHubNotifier.class)
@@ -21,6 +24,13 @@ public class GitHubNotifier {
       .web(WebApplicationType.NONE)
       .run(args)
       .getBean(ConfigurationService.class).load();
+  }
+
+  private ApplicationContext context;
+
+  @Autowired
+  public GitHubNotifier(ApplicationContext context) {
+    this.context = context;
   }
 
   @Bean
@@ -31,6 +41,15 @@ public class GitHubNotifier {
   @Bean
   public TaskScheduler taskScheduler() {
     return new ThreadPoolTaskScheduler();
+  }
+
+  @Override
+  public void run(ApplicationArguments args) throws Exception {
+    context.getBean(ConfigurationService.class).load();
+
+    if (args.containsOption("query-runner")) {
+      context.getBean(QueryRunner.class).run();
+    }
   }
 
 }
