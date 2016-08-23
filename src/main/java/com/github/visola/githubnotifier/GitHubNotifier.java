5098ef2aed87f8cc8ee1838af902a4b62e737420
Vinicius Isola <viniciusisola@gmail.com>
2016-08-22 20:21:53 -0400

    Add query runnuer

--- src/main/java/com/github/visola/githubnotifier/GitHubNotifier.java
+++ src/main/java/com/github/visola/githubnotifier/GitHubNotifier.java
@@ -4,6 +4,9 @@ import java.awt.AWTException;
 import java.io.IOException;
 
 import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ApplicationContext;
@@ -13,18 +16,24 @@ import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+import com.github.visola.githubnotifier.data.QueryRunner;
 import com.github.visola.githubnotifier.service.ConfigurationService;
 
 @SpringBootApplication
-public class GitHubNotifier extends WebMvcConfigurerAdapter {
+public class GitHubNotifier extends WebMvcConfigurerAdapter implements ApplicationRunner {
 
   public static void main(String[] args) throws BeansException, AWTException, IOException {
-    ApplicationContext context = new SpringApplicationBuilder(GitHubNotifier.class)
+    new SpringApplicationBuilder(GitHubNotifier.class)
       .headless(false)
       .web(false)
       .run(args);
+  }
 
-    context.getBean(ConfigurationService.class).load();
+  private ApplicationContext context;
+
+  @Autowired
+  public GitHubNotifier(ApplicationContext context) {
+    this.context = context;
   }
 
   @Bean
@@ -35,6 +44,15 @@ public class GitHubNotifier extends WebMvcConfigurerAdapter {
   @Bean
   public TaskScheduler taskScheduler() {
     return new ThreadPoolTaskScheduler();
+  }
+
+  @Override
+  public void run(ApplicationArguments args) throws Exception {
+    context.getBean(ConfigurationService.class).load();
+
+    if (args.containsOption("query-runner")) {
+      context.getBean(QueryRunner.class).run();
+    }
   }
 
 }
