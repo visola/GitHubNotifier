2837f4144da5d00230004c1a3825157c1b62dacb
Vinicius Isola <viniciusisola@gmail.com>
2019-08-06 16:37:11 -0400

    Make build fail on warnings and fix them

--- src/main/java/com/github/visola/githubnotifier/GitHubNotifier.java
+++ src/main/java/com/github/visola/githubnotifier/GitHubNotifier.java
@@ -8,13 +8,12 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import com.github.visola.githubnotifier.ui.SystemTrayManager;
 
 @EnableScheduling
 @SpringBootApplication
-public class GitHubNotifier extends WebMvcConfigurerAdapter {
+public class GitHubNotifier {
 
   public static void main(String[] args) throws BeansException {
     ApplicationContext context = new SpringApplicationBuilder(GitHubNotifier.class)
